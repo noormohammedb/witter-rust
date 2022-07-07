15a9ac8026adcf4e41a7961fb4394c86c9290fe5
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-07 17:34:47 +0530

    first setup
    - basic tide server
    - dependencies with features
    - custom error type

--- witter-backend/src/main.rs
+++ witter-backend/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use tide;
+
+#[async_std::main]
+async fn main() -> Result<(), CustomError> {
+    dotenv().ok();
+
+    // let db_url = std::env::var("DATABASE_URL").unwrap();
+    // dbg!(db_url);
+
+    let mut app = tide::new();
+    app.at("/").get(|_| async move { Ok("Hello, World!") });
+    app.listen("127.0.0.1:8080").await?;
+
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum CustomError {
+    #[error(transparent)]
+    DbError(#[from] sqlx::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
