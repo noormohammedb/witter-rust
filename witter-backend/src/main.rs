8c2535483951c1cad4ed3643077f8b9bf9050ede
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-07 18:38:39 +0530

    Database Connection
    - db connected with sqlx
    - logger implimented
    - test database

--- witter-backend/src/main.rs
+++ witter-backend/src/main.rs
@@ -1,12 +1,24 @@
 use dotenv::dotenv;
+use sqlx::pool::Pool;
+use sqlx::query;
+use sqlx::PgPool;
 use tide;
 
 #[async_std::main]
 async fn main() -> Result<(), CustomError> {
     dotenv().ok();
+    pretty_env_logger::init();
 
-    // let db_url = std::env::var("DATABASE_URL").unwrap();
     // dbg!(db_url);
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+
+    let db_pool: PgPool = Pool::connect(&db_url).await.unwrap();
+
+    let rows = query!("select (1) as id, 'Herp Derpinson' as name")
+        .fetch_one(&db_pool)
+        .await?;
+
+    dbg!(rows);
 
     let mut app = tide::new();
     app.at("/").get(|_| async move { Ok("Hello, World!") });
@@ -21,4 +33,6 @@ enum CustomError {
     DbError(#[from] sqlx::Error),
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    VarError(#[from] std::env::VarError),
 }
